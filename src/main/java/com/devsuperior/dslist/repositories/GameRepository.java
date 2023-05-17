439654e14367b0bf9f7dca11da77d34a9f186dde
Marcos Topini <marcostopini@gmail.com>
2023-05-17 19:58:53 -0300

    Domain model

--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.devsuperior.dslist.entities.Game;
 
 // Componente da camada e acesso a dados 
+// Game = referência para a entidade  Long = referência para o tipo do ID
 public interface GameRepository extends JpaRepository<Game, Long>{
 
 }
